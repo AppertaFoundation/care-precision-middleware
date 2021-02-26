654767e42c0822eb265a6830502251453d050f07
Alastair Douglas (altreus) <altreus@altre.us>
2021-02-26 20:17:11 +0000

    Put server back to sending 204 no content

--- app/server.pl
+++ app/server.pl
@@ -730,9 +730,9 @@ my $handler__cdr = POE::Session->create(
             }
 
             # Finally return the XML file so we can see the results
-            $frontend_response->header('Content-Type' => 'application/xml');
-            $frontend_response->content(encode_utf8($composition_obj->{output}));
-            $frontend_response->code(201);
+            #$frontend_response->header('Content-Type' => 'application/xml');
+            #$frontend_response->content(encode_utf8($composition_obj->{output}));
+            $frontend_response->code(204);
             $kernel->yield('finalize', $frontend_response);
         },
 
