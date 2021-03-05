cccfec5de7b3408921160a699fd5dd0a5ba205b9
Paul G Webster <paul.g.webster@googlemail.com>
2021-03-05 17:01:21 +0000

    Updated to using respiration_rate for ACME C19

--- app/server.pl
+++ app/server.pl
@@ -1862,7 +1862,7 @@ sub make_up_score {
         # NEED THESE:
 
         my $news2_scoring = $news2_calculator->news2_calculate_score({
-            'respiration_rate'          =>  $assessment->{news2}->{respirations}->{magnitude},
+            'respiration_rate'          =>  $assessment->{news2}->{respiration_rate}->{magnitude},
             'spo2_scale_1'              =>  $assessment->{news2}->{spo2},
             'pulse'                     =>  $assessment->{news2}->{pulse}->{magnitude},
             'temperature'               =>  $assessment->{news2}->{temperature}->{magnitude},
